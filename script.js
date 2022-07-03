b16f2b567643b84a68e409c132f624a4a1662da7
Jennat H <99939759+JennatL@users.noreply.github.com>
2022-07-03 15:04:23 -0400

    change section typing

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Data Scientist", "Statistician", "Developer"],
+        strings: ["Data Scientist", "Statistician", "Web Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Data Scientist", "Statistician", "Developer"],
+        strings: ["Data Scientist", "Statistician", "Web Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
